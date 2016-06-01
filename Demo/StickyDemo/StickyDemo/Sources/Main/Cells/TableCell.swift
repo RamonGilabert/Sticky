6a0e53f40a33c66a339c82a9c58f3da5ca01b9c3
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-06-01 13:53:42 +0200

    Adds the view model cells

--- Demo/StickyDemo/StickyDemo/Sources/Main/Cells/TableCell.swift
+++ Demo/StickyDemo/StickyDemo/Sources/Main/Cells/TableCell.swift
@@ -2,25 +2,45 @@ import UIKit
 
 class TableCell: UITableViewCell {
 
+  struct Dimensions {
+    struct Avatar {
+      static let size: CGFloat = 100
+      static let offset: CGFloat = 14
+    }
+  }
+
   lazy var avatarView: UIImageView = {
     let imageView = UIImageView()
+    imageView.contentMode = .ScaleAspectFit
+    imageView.clipsToBounds = true
+    imageView.layer.cornerRadius = Dimensions.Avatar.size / 2
+
     return imageView
   }()
 
   lazy var titleLabel: UILabel = {
     let label = UILabel()
+    label.numberOfLines = 0
+    label.font = UIFont.boldSystemFontOfSize(16)
+
     return label
   }()
 
   lazy var subtitleLabel: UILabel = {
     let label = UILabel()
+    label.numberOfLines = 0
+    label.font = UIFont.systemFontOfSize(14)
+
     return label
   }()
 
   override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-    [avatarView, titleLabel, subtitleLabel].forEach { addSubview($0) }
+    [avatarView, titleLabel, subtitleLabel].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      addSubview($0)
+    }
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -40,6 +60,13 @@ class TableCell: UITableViewCell {
   // MARK: - Setup constraints
 
   func setupConstraints() {
-    
+    NSLayoutConstraint.activateConstraints([
+      avatarView.widthAnchor.constraintEqualToConstant(Dimensions.Avatar.size),
+      avatarView.heightAnchor.constraintEqualToConstant(Dimensions.Avatar.size),
+      avatarView.topAnchor.constraintEqualToAnchor(topAnchor, constant: Dimensions.Avatar.offset),
+      avatarView.leftAnchor.constraintEqualToAnchor(leftAnchor, constant: Dimensions.Avatar.offset),
+
+      
+      ])
   }
 }
--- Demo/StickyDemo/StickyDemo/Sources/Main/ViewModels/TableViewModel.swift
+++ Demo/StickyDemo/StickyDemo/Sources/Main/ViewModels/TableViewModel.swift
@@ -5,4 +5,60 @@ struct TableViewModel {
   var image: UIImage? = nil
   var title: String = ""
   var subtitle: String = ""
+
+  static var cells = [
+    TableViewModel(
+      image: nil,
+      title: "Truffaut normcore",
+      subtitle: "Aesthetic fashion axe 3 wolf moon hashtag, jean shorts kitsch pinterest normcore cliche. " +
+      "Squid jean shorts venmo lomo biodiesel, marfa raw denim yuccie sriracha twee pug ramps."),
+
+    TableViewModel(
+      image: nil,
+      title: "Organic austin",
+      subtitle: "Flannel VHS hammock, kitsch scenester keffiyeh tacos shabby chic " +
+      "letterpress butcher offal echo park raw denim."),
+
+    TableViewModel(
+      image: nil,
+      title: "Green juice",
+      subtitle: "Tacos YOLO locavore fingerstache, bitters street art ethical actually artisan " +
+      "pork belly yr franzen PBR&B polaroid. Mumblecore disrupt butcher cold-pressed pork belly."),
+
+    TableViewModel(
+      image: nil,
+      title: "Chicharrones dreamcatcher",
+      subtitle: "Trust fund pickled gentrify, jean shorts man bun pug everyday carry gluten-free. " +
+      "Listicle meh aesthetic, gentrify neutra sartorial distillery waistcoat."),
+
+    TableViewModel(
+      image: nil,
+      title: "Offal banh",
+      subtitle: "Flannel VHS hammock, kitsch scenester keffiyeh tacos shabby chic letterpress " +
+      "butcher offal echo park raw denim"),
+
+    TableViewModel(
+      image: nil,
+      title: "Tumblr austin",
+      subtitle: "Tacos YOLO locavore fingerstache, bitters street art ethical actually artisan " +
+      "pork belly yr franzen PBR&B polaroid. Mumblecore disrupt butcher cold-pressed pork belly."),
+
+    TableViewModel(
+      image: nil,
+      title: "Aesthetic fashion",
+      subtitle: "Flannel VHS hammock, kitsch scenester keffiyeh tacos shabby chic letterpress " +
+      "butcher offal echo park raw denim"),
+
+    TableViewModel(
+      image: nil,
+      title: "Hashtag chambray",
+      subtitle: "Trust fund pickled gentrify, jean shorts man bun pug everyday carry gluten-free. " +
+      "Listicle meh aesthetic, gentrify neutra sartorial distillery waistcoat."),
+
+    TableViewModel(
+      image: nil,
+      title: "Hashtag sticky",
+      subtitle: "Tacos YOLO locavore fingerstache, bitters street art ethical actually artisan " +
+      "pork belly yr franzen PBR&B polaroid. Mumblecore disrupt butcher cold-pressed pork belly.")
+  ]
 }
